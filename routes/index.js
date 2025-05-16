7817a877816e0ee82abcc78047dc98e1b38ad291
chooyee <chooyee@gmail.com>
2025-05-16 11:48:08 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,21 @@ router.get("/", (req, res) => {
 	res.render("index");
 });
 
+router.get("/server", (req, res) => {
+	const hostname =
+		process.env.ENVIRONMENT === "dev"
+			? `${req.protocol}://${req.header("host")}`
+			: `${req.protocol}://${req.hostname}`;
+	res.render("server2");
+});
+
+router.get("/cert", (req, res) => {
+	const hostname =
+		process.env.ENVIRONMENT === "dev"
+			? `${req.protocol}://${req.header("host")}`
+			: `${req.protocol}://${req.hostname}`;
+	res.render("cert");
+});
 
 // Routes
 router.post("/upload", upload.single("file"), async (req, res) => {
@@ -32,17 +47,38 @@ router.post("/upload", upload.single("file"), async (req, res) => {
   */
 	try{
 		const fsService = new FileStorageService(req.file.originalname, req.file.buffer);
-		const metadata = await fsService.StoreSecretFile();
-		console.log(JSON.stringify(metadata));
+		const result = await fsService.StoreSecretFile();
+		console.log(JSON.stringify(result));
 		const svcbus = new AzureSvcBusService(process.env.AZURE_SVCBUS_NAMESPACE, process.env.AZURE_SVCBUS_QUEUE);
-		await svcbus.SendJson(JSON.stringify(metadata));
-		res.status(200).json(metadata);
+		await svcbus.SendJson(JSON.stringify(result));
+		res.status(200).json(result);
 	}
 	catch(error)
 	{
 		res.status(500).json({"Error":error});
 	}
+	//==============================================================================
+	//Glue 
+	//==============================================================================
+	// const glue = [];
+	// for (let x=0;x<i;x++)
+	// {
+	// 	const fileName = `./download/encrypted_part_${x + 1}.dat`;
+	// 	glue.push(fs.readFileSync(fileName));
+		
+	// }
+	// const gluedFile = Buffer.concat(glue);
+	// const gluedFileName = "./download/gluedfile.dat";
+	// fs.writeFileSync(gluedFileName, gluedFile);
 
+	// descryptRes = await AzureKeyFactory.DecryptFile("test", result.encryptedSymmetricKey,result.iv,gluedFileName);
+	// fs.writeFileSync("./download/" + req.file.filename,  Buffer.from(descryptRes, "base64"));
+	//==============================================================================
+	//end GLue
+	//==============================================================================
+
+	
+	
 });
 
 
